c762b70b832eb5516e3f119bd801b090aafcec61
Giovani <giovani98@proton.me>
2024-08-08 14:16:37 -0300

    feat: Add room creation page

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,22 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CreateRoom />,
+  },
+  {
+    path: '/room/:roomId',
+    element: <Room />,
+  },
+  {
+    path: '*',
+    element: <h1>404 Page Not Found</h1>,
+  },
+])
+
 export function App() {
-  return <h1>Here</h1>
+  return <RouterProvider router={router} />
 }
